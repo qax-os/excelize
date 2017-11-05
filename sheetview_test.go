88e48e079a91191e05f32a232f8dec4454b25238
Olivier Mengué <dolmen@cpan.org>
2017-11-05 02:16:41 +0100

    Add SetSheetViewOptions and GetSheetViewOptions (#145)
    Two new methods:
    - SetSheetViewOptions(sheetName string, viewIndex int, opts ...SheetViewOption) error
    - GetSheetViewOptions(sheetName string, viewIndex int, opts ...SheetViewOptionPtr) error
    
    The option values are given by the user through types that have privates methods
    that implement the private SheetViewOption and SheetViewOptionPtr interfaces:
    - DefaultGridColor(bool)
    - RightToLeft(bool)
    - ShowFormulas(bool)
    - ShowGridLines(bool)
    - ShowRowColHeaders(bool)
    
    Examples:
        err := xl.SetSheetViewOptions("Sheet1", -1, excelize.ShowGridLines(true))
    
        var showGridLines excelize.ShowGridLines
        err := xl.GetSheetViewOptions("Sheet1", -1, &showGridLines)
    
    Fixes #145.

---
+++ sheetview_test.go
@@ -0,0 +1,122 @@
+package excelize_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+)
+
+var _ = []excelize.SheetViewOption{
+	excelize.DefaultGridColor(true),
+	excelize.RightToLeft(false),
+	excelize.ShowFormulas(false),
+	excelize.ShowGridLines(true),
+	excelize.ShowRowColHeaders(true),
+}
+
+var _ = []excelize.SheetViewOptionPtr{
+	(*excelize.DefaultGridColor)(nil),
+	(*excelize.RightToLeft)(nil),
+	(*excelize.ShowFormulas)(nil),
+	(*excelize.ShowGridLines)(nil),
+	(*excelize.ShowRowColHeaders)(nil),
+}
+
+func ExampleFile_SetSheetViewOptions() {
+	xl := excelize.NewFile()
+	const sheet = "Sheet1"
+
+	if err := xl.SetSheetViewOptions(sheet, 0,
+		excelize.DefaultGridColor(false),
+		excelize.RightToLeft(false),
+		excelize.ShowFormulas(true),
+		excelize.ShowGridLines(true),
+		excelize.ShowRowColHeaders(true),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+}
+
+func ExampleFile_GetSheetViewOptions() {
+	xl := excelize.NewFile()
+	const sheet = "Sheet1"
+
+	var (
+		defaultGridColor  excelize.DefaultGridColor
+		rightToLeft       excelize.RightToLeft
+		showFormulas      excelize.ShowFormulas
+		showGridLines     excelize.ShowGridLines
+		showRowColHeaders excelize.ShowRowColHeaders
+	)
+
+	if err := xl.GetSheetViewOptions(sheet, 0,
+		&defaultGridColor,
+		&rightToLeft,
+		&showFormulas,
+		&showGridLines,
+		&showRowColHeaders,
+	); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Default:")
+	fmt.Println("- defaultGridColor:", defaultGridColor)
+	fmt.Println("- rightToLeft:", rightToLeft)
+	fmt.Println("- showFormulas:", showFormulas)
+	fmt.Println("- showGridLines:", showGridLines)
+	fmt.Println("- showRowColHeaders:", showRowColHeaders)
+
+	if err := xl.SetSheetViewOptions(sheet, 0, excelize.ShowGridLines(false)); err != nil {
+		panic(err)
+	}
+
+	if err := xl.GetSheetViewOptions(sheet, 0, &showGridLines); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("After change:")
+	fmt.Println("- showGridLines:", showGridLines)
+
+	// Output:
+	// Default:
+	// - defaultGridColor: true
+	// - rightToLeft: false
+	// - showFormulas: false
+	// - showGridLines: true
+	// - showRowColHeaders: true
+	// After change:
+	// - showGridLines: false
+}
+
+func TestSheetViewOptionsErrors(t *testing.T) {
+	xl := excelize.NewFile()
+	const sheet = "Sheet1"
+
+	if err := xl.GetSheetViewOptions(sheet, 0); err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+	if err := xl.GetSheetViewOptions(sheet, -1); err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+	if err := xl.GetSheetViewOptions(sheet, 1); err == nil {
+		t.Error("Error expected but got nil")
+	}
+	if err := xl.GetSheetViewOptions(sheet, -2); err == nil {
+		t.Error("Error expected but got nil")
+	}
+
+	if err := xl.SetSheetViewOptions(sheet, 0); err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+	if err := xl.SetSheetViewOptions(sheet, -1); err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+	if err := xl.SetSheetViewOptions(sheet, 1); err == nil {
+		t.Error("Error expected but got nil")
+	}
+	if err := xl.SetSheetViewOptions(sheet, -2); err == nil {
+		t.Error("Error expected but got nil")
+	}
+}
