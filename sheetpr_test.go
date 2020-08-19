b57a864fb543edd72cfe36c30d4ebdb1f2f55610
Eugene Androsov <e.androsov@paser.io>
2020-08-19 15:26:42 +0300

    Add different pivot options Add header options to pivot table opts Add Style name options to pivot table opts

--- sheetpr_test.go
+++ sheetpr_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mohae/deepcopy"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"github.com/EugeneAndrosovPaser/excelize/v2"
 )
 
 var _ = []excelize.SheetPrOption{
