bafe087a61ca85b16fc69fda280f03eb2d7551dc
jaby <peter.de.velder@ilias-solutions.com>
2021-06-08 13:02:34 +0200

    This closes #858 (#859)
    * fixes https://github.com/360EntSecGroup-Skylar/excelize/issues/858
    
    * fixes https://github.com/360EntSecGroup-Skylar/excelize/issues/858
    
    Co-authored-by: dvelderp <peter.de.velder@gmail.com>

--- calc.go
+++ calc.go
@@ -628,16 +628,10 @@ func (f *File) evalInfixExp(sheet, cell string, tokens []efp.Token) (efp.Token, 
 			}
 
 			// current token is logical
-			if token.TType == efp.OperatorsInfix && token.TSubType == efp.TokenSubTypeLogical {
-			}
 			if token.TType == efp.TokenTypeOperand && token.TSubType == efp.TokenSubTypeLogical {
 				argsStack.Peek().(*list.List).PushBack(newStringFormulaArg(token.TValue))
 			}
 
-			// current token is text
-			if token.TType == efp.TokenTypeOperand && token.TSubType == efp.TokenSubTypeText {
-				argsStack.Peek().(*list.List).PushBack(newStringFormulaArg(token.TValue))
-			}
 			if err = f.evalInfixExpFunc(sheet, cell, token, nextToken, opfStack, opdStack, opftStack, opfdStack, argsStack); err != nil {
 				return efp.Token{}, err
 			}
@@ -1012,7 +1006,7 @@ func (f *File) parseToken(sheet string, token efp.Token, opdStack, optStack *Sta
 		optStack.Pop()
 	}
 	// opd
-	if token.TType == efp.TokenTypeOperand && token.TSubType == efp.TokenSubTypeNumber {
+	if token.TType == efp.TokenTypeOperand && (token.TSubType == efp.TokenSubTypeNumber || token.TSubType == efp.TokenSubTypeText) {
 		opdStack.Push(token)
 	}
 	return nil
