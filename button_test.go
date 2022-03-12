750ca854310ce3e477c60a9f8dcdcc5c2ac8b8c7
jdavidvr <jdavidvr@hotmail.com>
2022-03-12 01:51:11 -0400

    fixed test errors

--- button_test.go
+++ button_test.go
@@ -1,7 +1,6 @@
 package excelize
 
 import (
-	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,10 +15,7 @@ func TestAddButton(t *testing.T) {
 	assert.NoError(t, f.AddButton("Sheet1", "A30", `{"macro": "say_hello","caption": "Press Me","width": 80,"height": 30}`))
 
 	// Test add button on not exists worksheet.
-	assert.EqualError(t, f.AddComment("SheetN", "B7", `{"macro": "say_hello","caption": "Press Me","width": 80,"height": 30}`), "sheet SheetN is not exist")
+	assert.EqualError(t, f.AddButton("SheetN", "B7", `{"macro": "say_hello","caption": "Press Me","width": 80,"height": 30}`), "sheet SheetN is not exist")
 	// Test add button on with illegal cell coordinates
-	assert.EqualError(t, f.AddComment("Sheet1", "A", `{"macro": "say_hello","caption": "Press Me","width": 80,"height": 30}`), newCellNameToCoordinatesError("A", newInvalidCellNameError("A")).Error())
-	if assert.NoError(t, f.SaveAs(filepath.Join("test", "TestAddButton.xlsx"))) {
-		assert.Len(t, f.GetComments(), 2)
-	}
+	assert.EqualError(t, f.AddButton("Sheet1", "A", `{"macro": "say_hello","caption": "Press Me","width": 80,"height": 30}`), newCellNameToCoordinatesError("A", newInvalidCellNameError("A")).Error())
 }
