f707b2d2da77ff3715ab729434b59f374b6e8c94
张涛 <44195851+doingNobb@users.noreply.github.com>
2023-03-01 13:25:17 +0800

    This closes #1484, fix the formula calc result issue (#1485)
    - Optimize variable name for data validation

--- datavalidation_test.go
+++ datavalidation_test.go
@@ -25,13 +25,13 @@ func TestDataValidation(t *testing.T) {
 
 	f := NewFile()
 
-	dvRange := NewDataValidation(true)
-	dvRange.Sqref = "A1:B2"
-	assert.NoError(t, dvRange.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorBetween))
-	dvRange.SetError(DataValidationErrorStyleStop, "error title", "error body")
-	dvRange.SetError(DataValidationErrorStyleWarning, "error title", "error body")
-	dvRange.SetError(DataValidationErrorStyleInformation, "error title", "error body")
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	dv := NewDataValidation(true)
+	dv.Sqref = "A1:B2"
+	assert.NoError(t, dv.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorBetween))
+	dv.SetError(DataValidationErrorStyleStop, "error title", "error body")
+	dv.SetError(DataValidationErrorStyleWarning, "error title", "error body")
+	dv.SetError(DataValidationErrorStyleInformation, "error title", "error body")
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 
 	dataValidations, err := f.GetDataValidations("Sheet1")
 	assert.NoError(t, err)
@@ -39,11 +39,11 @@ func TestDataValidation(t *testing.T) {
 
 	assert.NoError(t, f.SaveAs(resultFile))
 
-	dvRange = NewDataValidation(true)
-	dvRange.Sqref = "A3:B4"
-	assert.NoError(t, dvRange.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorGreaterThan))
-	dvRange.SetInput("input title", "input body")
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	dv = NewDataValidation(true)
+	dv.Sqref = "A3:B4"
+	assert.NoError(t, dv.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorGreaterThan))
+	dv.SetInput("input title", "input body")
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 
 	dataValidations, err = f.GetDataValidations("Sheet1")
 	assert.NoError(t, err)
@@ -55,11 +55,11 @@ func TestDataValidation(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NoError(t, f.SetSheetRow("Sheet2", "A2", &[]interface{}{"B2", 1}))
 	assert.NoError(t, f.SetSheetRow("Sheet2", "A3", &[]interface{}{"B3", 3}))
-	dvRange = NewDataValidation(true)
-	dvRange.Sqref = "A1:B1"
-	assert.NoError(t, dvRange.SetRange("INDIRECT($A$2)", "INDIRECT($A$3)", DataValidationTypeWhole, DataValidationOperatorBetween))
-	dvRange.SetError(DataValidationErrorStyleStop, "error title", "error body")
-	assert.NoError(t, f.AddDataValidation("Sheet2", dvRange))
+	dv = NewDataValidation(true)
+	dv.Sqref = "A1:B1"
+	assert.NoError(t, dv.SetRange("INDIRECT($A$2)", "INDIRECT($A$3)", DataValidationTypeWhole, DataValidationOperatorBetween))
+	dv.SetError(DataValidationErrorStyleStop, "error title", "error body")
+	assert.NoError(t, f.AddDataValidation("Sheet2", dv))
 	dataValidations, err = f.GetDataValidations("Sheet1")
 	assert.NoError(t, err)
 	assert.Equal(t, len(dataValidations), 2)
@@ -67,8 +67,8 @@ func TestDataValidation(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, len(dataValidations), 1)
 
-	dvRange = NewDataValidation(true)
-	dvRange.Sqref = "A5:B6"
+	dv = NewDataValidation(true)
+	dv.Sqref = "A5:B6"
 	for _, listValid := range [][]string{
 		{"1", "2", "3"},
 		{strings.Repeat("&", MaxFieldLength)},
@@ -76,14 +76,14 @@ func TestDataValidation(t *testing.T) {
 		{strings.Repeat("\U0001F600", 100), strings.Repeat("\u4E01", 50), "<&>"},
 		{`A<`, `B>`, `C"`, "D\t", `E'`, `F`},
 	} {
-		dvRange.Formula1 = ""
-		assert.NoError(t, dvRange.SetDropList(listValid),
+		dv.Formula1 = ""
+		assert.NoError(t, dv.SetDropList(listValid),
 			"SetDropList failed for valid input %v", listValid)
-		assert.NotEqual(t, "", dvRange.Formula1,
+		assert.NotEqual(t, "", dv.Formula1,
 			"Formula1 should not be empty for valid input %v", listValid)
 	}
-	assert.Equal(t, `<formula1>"A&lt;,B&gt;,C"",D	,E',F"</formula1>`, dvRange.Formula1)
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	assert.Equal(t, `<formula1>"A&lt;,B&gt;,C"",D	,E',F"</formula1>`, dv.Formula1)
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 
 	dataValidations, err = f.GetDataValidations("Sheet1")
 	assert.NoError(t, err)
@@ -113,29 +113,29 @@ func TestDataValidationError(t *testing.T) {
 	assert.NoError(t, f.SetCellStr("Sheet1", "E2", "E2"))
 	assert.NoError(t, f.SetCellStr("Sheet1", "E3", "E3"))
 
-	dvRange := NewDataValidation(true)
-	dvRange.SetSqref("A7:B8")
-	dvRange.SetSqref("A7:B8")
-	dvRange.SetSqrefDropList("$E$1:$E$3")
+	dv := NewDataValidation(true)
+	dv.SetSqref("A7:B8")
+	dv.SetSqref("A7:B8")
+	dv.SetSqrefDropList("$E$1:$E$3")
 
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 
-	dvRange = NewDataValidation(true)
-	err := dvRange.SetDropList(make([]string, 258))
-	if dvRange.Formula1 != "" {
+	dv = NewDataValidation(true)
+	err := dv.SetDropList(make([]string, 258))
+	if dv.Formula1 != "" {
 		t.Errorf("data validation error. Formula1 must be empty!")
 		return
 	}
 	assert.EqualError(t, err, ErrDataValidationFormulaLength.Error())
-	assert.EqualError(t, dvRange.SetRange(nil, 20, DataValidationTypeWhole, DataValidationOperatorBetween), ErrParameterInvalid.Error())
-	assert.EqualError(t, dvRange.SetRange(10, nil, DataValidationTypeWhole, DataValidationOperatorBetween), ErrParameterInvalid.Error())
-	assert.NoError(t, dvRange.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorGreaterThan))
-	dvRange.SetSqref("A9:B10")
+	assert.EqualError(t, dv.SetRange(nil, 20, DataValidationTypeWhole, DataValidationOperatorBetween), ErrParameterInvalid.Error())
+	assert.EqualError(t, dv.SetRange(10, nil, DataValidationTypeWhole, DataValidationOperatorBetween), ErrParameterInvalid.Error())
+	assert.NoError(t, dv.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorGreaterThan))
+	dv.SetSqref("A9:B10")
 
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 
 	// Test width invalid data validation formula
-	prevFormula1 := dvRange.Formula1
+	prevFormula1 := dv.Formula1
 	for _, keys := range [][]string{
 		make([]string, 257),
 		{strings.Repeat("s", 256)},
@@ -143,19 +143,19 @@ func TestDataValidationError(t *testing.T) {
 		{strings.Repeat("\U0001F600", 128)},
 		{strings.Repeat("\U0001F600", 127), "s"},
 	} {
-		err = dvRange.SetDropList(keys)
-		assert.Equal(t, prevFormula1, dvRange.Formula1,
+		err = dv.SetDropList(keys)
+		assert.Equal(t, prevFormula1, dv.Formula1,
 			"Formula1 should be unchanged for invalid input %v", keys)
 		assert.EqualError(t, err, ErrDataValidationFormulaLength.Error())
 	}
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
-	assert.NoError(t, dvRange.SetRange(
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
+	assert.NoError(t, dv.SetRange(
 		-math.MaxFloat32, math.MaxFloat32,
 		DataValidationTypeWhole, DataValidationOperatorGreaterThan))
-	assert.EqualError(t, dvRange.SetRange(
+	assert.EqualError(t, dv.SetRange(
 		-math.MaxFloat64, math.MaxFloat32,
 		DataValidationTypeWhole, DataValidationOperatorGreaterThan), ErrDataValidationRange.Error())
-	assert.EqualError(t, dvRange.SetRange(
+	assert.EqualError(t, dv.SetRange(
 		math.SmallestNonzeroFloat64, math.MaxFloat64,
 		DataValidationTypeWhole, DataValidationOperatorGreaterThan), ErrDataValidationRange.Error())
 	assert.NoError(t, f.SaveAs(resultFile))
@@ -173,33 +173,33 @@ func TestDeleteDataValidation(t *testing.T) {
 	f := NewFile()
 	assert.NoError(t, f.DeleteDataValidation("Sheet1", "A1:B2"))
 
-	dvRange := NewDataValidation(true)
-	dvRange.Sqref = "A1:B2"
-	assert.NoError(t, dvRange.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorBetween))
-	dvRange.SetInput("input title", "input body")
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	dv := NewDataValidation(true)
+	dv.Sqref = "A1:B2"
+	assert.NoError(t, dv.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorBetween))
+	dv.SetInput("input title", "input body")
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 	assert.NoError(t, f.DeleteDataValidation("Sheet1", "A1:B2"))
 
-	dvRange.Sqref = "A1"
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	dv.Sqref = "A1"
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 	assert.NoError(t, f.DeleteDataValidation("Sheet1", "B1"))
 	assert.NoError(t, f.DeleteDataValidation("Sheet1", "A1"))
 
-	dvRange.Sqref = "C2:C5"
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	dv.Sqref = "C2:C5"
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 	assert.NoError(t, f.DeleteDataValidation("Sheet1", "C4"))
 
-	dvRange = NewDataValidation(true)
-	dvRange.Sqref = "D2:D2 D3 D4"
-	assert.NoError(t, dvRange.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorBetween))
-	dvRange.SetInput("input title", "input body")
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	dv = NewDataValidation(true)
+	dv.Sqref = "D2:D2 D3 D4"
+	assert.NoError(t, dv.SetRange(10, 20, DataValidationTypeWhole, DataValidationOperatorBetween))
+	dv.SetInput("input title", "input body")
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 	assert.NoError(t, f.DeleteDataValidation("Sheet1", "D3"))
 
 	assert.NoError(t, f.SaveAs(filepath.Join("test", "TestDeleteDataValidation.xlsx")))
 
-	dvRange.Sqref = "A"
-	assert.NoError(t, f.AddDataValidation("Sheet1", dvRange))
+	dv.Sqref = "A"
+	assert.NoError(t, f.AddDataValidation("Sheet1", dv))
 	assert.EqualError(t, f.DeleteDataValidation("Sheet1", "A1"), newCellNameToCoordinatesError("A", newInvalidCellNameError("A")).Error())
 
 	assert.EqualError(t, f.DeleteDataValidation("Sheet1", "A1:A"), newCellNameToCoordinatesError("A", newInvalidCellNameError("A")).Error())
