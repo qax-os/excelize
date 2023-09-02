f47f96330162a7467ef824a817e87fa77e517f64
Francis Nickels <fnickels@riotgames.com>
2023-09-02 10:16:53 -0700

    add Footer & Header clarification to docs

--- sheet.go
+++ sheet.go
@@ -1123,8 +1123,8 @@ func attrValToBool(name string, attrs []xml.Attr) (val bool, err error) {
 //	 DifferentFirst   | Different first-page header and footer indicator
 //	 DifferentOddEven | Different odd and even page headers and footers indicator
 //	 ScaleWithDoc     | Scale header and footer with document scaling
-//	 OddFooter        | Odd Page Footer
-//	 OddHeader        | Odd Header
+//	 OddFooter        | Odd Page Footer, or primary Page Footer if 'DifferentOddEven' is 'false'
+//	 OddHeader        | Odd Header, or primary Page Header if 'DifferentOddEven' is 'false'
 //	 EvenFooter       | Even Page Footer
 //	 EvenHeader       | Even Page Header
 //	 FirstFooter      | First Page Footer
