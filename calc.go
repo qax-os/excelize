6e6ce35a98b7c34848c0c469d90cf3f8a535673a
Vivek Kairi <vivek@zerodhafundhouse.com>
2024-02-15 09:33:54 +0530

    CalcCellValue ordering fix

--- calc.go
+++ calc.go
@@ -805,12 +805,12 @@ type formulaFuncs struct {
 //	Z.TEST
 //	ZTEST
 func (f *File) CalcCellValue(sheet, cell string, opts ...Options) (result string, err error) {
+	opts = append([]Options{*f.options}, opts...)
 	var (
 		rawCellValue = getOptions(opts...).RawCellValue
 		styleIdx     int
 		token        formulaArg
 	)
-	opts = append([]Options{*f.options}, opts...)
 	if token, err = f.calcCellValue(&calcContext{
 		entry:             fmt.Sprintf("%s!%s", sheet, cell),
 		maxCalcIterations: getOptions(opts...).MaxCalcIterations,
