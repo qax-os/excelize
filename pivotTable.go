13b714ad8fb23ca41c557215b8a0dc897ebbd79e
xuri <xuri.me@gmail.com>
2024-07-13 10:41:57 +0800

    This closes #1945, an error will be return if column header cell is empty in pivot table data range
    - Update unit tests

--- pivotTable.go
+++ pivotTable.go
@@ -260,6 +260,9 @@ func (f *File) getTableFieldsOrder(opts *PivotTableOptions) ([]string, error) {
 		if err != nil {
 			return order, err
 		}
+		if name == "" {
+			return order, ErrParameterInvalid
+		}
 		order = append(order, name)
 	}
 	return order, nil
@@ -272,8 +275,10 @@ func (f *File) addPivotCache(opts *PivotTableOptions) error {
 	if err != nil {
 		return newPivotTableDataRangeError(err.Error())
 	}
-	// data range has been checked
-	order, _ := f.getTableFieldsOrder(opts)
+	order, err := f.getTableFieldsOrder(opts)
+	if err != nil {
+		return newPivotTableDataRangeError(err.Error())
+	}
 	topLeftCell, _ := CoordinatesToCellName(coordinates[0], coordinates[1])
 	bottomRightCell, _ := CoordinatesToCellName(coordinates[2], coordinates[3])
 	pc := xlsxPivotCacheDefinition{
