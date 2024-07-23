8f6719b6c19f0d8a3b25f3d17211d675736682a5
xuri <xuri.me@gmail.com>
2024-07-23 21:48:17 +0800

    This close #1963, prevent the GetStyle function panic when theme without sysClr

--- xmlTheme.go
+++ xmlTheme.go
@@ -148,7 +148,7 @@ type xlsxEffectStyleLst struct {
 	EffectStyleLst string `xml:",innerxml"`
 }
 
-// xlsxBgFillStyleLst  element defines a list of background fills that are
+// xlsxBgFillStyleLst element defines a list of background fills that are
 // used within a theme. The background fills consist of three fills, arranged
 // in order from subtle to moderate to intense.
 type xlsxBgFillStyleLst struct {
