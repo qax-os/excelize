qax-os/excelize

commit 7715c1462a917c657d4022a4fe5b57d41d77055a

This closes #1886, remove the namespace prefix for the default spreadsheet namespace
- Improvement compatibility for the workbook internal part with a spreadsheet namespace prefix
- Update GitHub Action configuration, using the macOS 13 in the unit test pipeline to temporarily resolve test failed in macos-14-arm64

lib.go
@@ -646,6 +646,11 @@ func getRootElement(d *xml.Decoder) []xml.Attr {
 		case xml.StartElement:
 			tokenIdx++
 			if tokenIdx == 1 {
+				for i := 0; i < len(startElement.Attr); i++ {
+					if startElement.Attr[i].Value == NameSpaceSpreadSheet.Value {
+						startElement.Attr[i] = NameSpaceSpreadSheet
+					}
+				}
 				return startElement.Attr
 			}
 		}

.github/workflows/go.yml
@@ -6,7 +6,7 @@ jobs:
     strategy:
       matrix:
         go-version: [1.18.x, 1.19.x, 1.20.x, '>=1.21.1', 1.22.x]
-        os: [ubuntu-latest, macos-latest, windows-latest]
+        os: [ubuntu-latest, macos-13, windows-latest]
         targetplatform: [x86, x64]
 
     runs-on: ${{ matrix.os }}

commit 0d5d1c53b2bd33c68784f19fb8a4324ed68f372e

This closes #2015, fix a v2.9.0 regression bug introduced by commit 7715c1462a917c657d4022a4fe5b57d41d77055a
- Fix corrupted workbook generated by open the workbook generated by stream writer
- Update unit tests

lib.go
@@ -652,10 +652,15 @@ func getRootElement(d *xml.Decoder) []xml.Attr {
 		case xml.StartElement:
 			tokenIdx++
 			if tokenIdx == 1 {
+				var ns bool
 				for i := 0; i < len(startElement.Attr); i++ {
-					if startElement.Attr[i].Value == NameSpaceSpreadSheet.Value {
-						startElement.Attr[i] = NameSpaceSpreadSheet
+					if startElement.Attr[i].Value == NameSpaceSpreadSheet.Value &&
+						startElement.Attr[i].Name == NameSpaceSpreadSheet.Name {
+						ns = true
 					}
+				}
+				if !ns {
+					startElement.Attr = append(startElement.Attr, NameSpaceSpreadSheet)
 				}
 				return startElement.Attr
 			}

lib_test.go
@@ -289,6 +289,10 @@ func TestBytesReplace(t *testing.T) {
 
 func TestGetRootElement(t *testing.T) {
 	assert.Len(t, getRootElement(xml.NewDecoder(strings.NewReader(""))), 0)
+	// Test get workbook root element which all workbook XML namespace has prefix
+	f := NewFile()
+	d := f.xmlNewDecoder(bytes.NewReader([]byte(`<x:workbook xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:x="http://schemas.openxmlformats.org/spreadsheetml/2006/main"></x:workbook>`)))
+	assert.Len(t, getRootElement(d), 3)
 }
 
 func TestSetIgnorableNameSpace(t *testing.T) {

pivotTable_test.go
@@ -516,7 +516,7 @@ func TestDeleteWorkbookPivotCache(t *testing.T) {
 	f := NewFile()
 	// Test delete workbook pivot table cache with unsupported workbook charset
 	f.WorkBook = nil
-	f.Pkg.Store("xl/workbook.xml", MacintoshCyrillicCharset)
+	f.Pkg.Store(defaultXMLPathWorkbook, MacintoshCyrillicCharset)
 	assert.EqualError(t, f.deleteWorkbookPivotCache(PivotTableOptions{pivotCacheXML: "pivotCache/pivotCacheDefinition1.xml"}), "XML syntax error on line 1: invalid UTF-8")
 
 	// Test delete workbook pivot table cache with unsupported workbook relationships charset

sheet_test.go
@@ -580,7 +580,7 @@ func TestMoveSheet(t *testing.T) {
 
 	// Test move sheet with unsupported workbook charset
 	f.WorkBook = nil
-	f.Pkg.Store("xl/workbook.xml", MacintoshCyrillicCharset)
+	f.Pkg.Store(defaultXMLPathWorkbook, MacintoshCyrillicCharset)
 	assert.EqualError(t, f.MoveSheet("Sheet2", "Sheet1"), "XML syntax error on line 1: invalid UTF-8")
 }
 

slicer_test.go
@@ -597,7 +597,7 @@ func TestAddWorkbookSlicerCache(t *testing.T) {
 	// Test add a workbook slicer cache with unsupported charset workbook
 	f := NewFile()
 	f.WorkBook = nil
-	f.Pkg.Store("xl/workbook.xml", MacintoshCyrillicCharset)
+	f.Pkg.Store(defaultXMLPathWorkbook, MacintoshCyrillicCharset)
 	assert.EqualError(t, f.addWorkbookSlicerCache(1, ExtURISlicerCachesX15), "XML syntax error on line 1: invalid UTF-8")
 	assert.NoError(t, f.Close())
 }