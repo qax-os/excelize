860f88ed50440cc4e9c92a28c013777ecf25898f
R3dByt3 <marvin.gerdel@web.de>
2025-04-28 11:26:04 +0200

    [BUGFIX]: Fixed aspect ratio calculation of drawing resize

--- picture.go
+++ picture.go
@@ -753,13 +753,16 @@ func (f *File) drawingResize(sheet, cell string, width, height float64, opts *Gr
 			cellHeight += f.getRowHeight(sheet, row)
 		}
 	}
-	if float64(cellWidth) < width {
-		asp := float64(cellWidth) / width
-		width, height = float64(cellWidth), height*asp
-	}
-	if float64(cellHeight) < height {
-		asp := float64(cellHeight) / height
-		height, width = float64(cellHeight), width*asp
+	if float64(cellWidth) < width || float64(cellHeight) < height {
+		aspWidth := float64(cellWidth) / width
+		aspHeight := float64(cellHeight) / height
+		var asp float64
+		if aspWidth < aspHeight {
+			asp = aspWidth
+		} else {
+			asp = aspHeight
+		}
+		width, height = width*asp, height*asp
 	}
 	if opts.AutoFitIgnoreAspect {
 		width, height = float64(cellWidth), float64(cellHeight)
