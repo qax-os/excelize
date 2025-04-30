ddf39701d8461958ebde1d6d608c02928cbd4762
R3dByt3 <marvin.gerdel@web.de>
2025-04-30 14:02:15 +0200

    [MISC]: Fixed drawing dimensions

--- drawing.go
+++ drawing.go
@@ -1392,7 +1392,7 @@ func (f *File) addDrawingChart(sheet, drawingXML, cell string, width, height, rI
 	}
 	width = int(float64(width) * opts.ScaleX)
 	height = int(float64(height) * opts.ScaleY)
-	colStart, rowStart, colEnd, rowEnd, x1, y1, x2, y2 := f.positionObjectPixels(sheet, col, row, opts.OffsetX, opts.OffsetY, width, height)
+	colStart, rowStart, colEnd, rowEnd, x1, y1, x2, y2 := f.positionObjectPixels(sheet, opts.Positioning, col, row, opts.OffsetX, opts.OffsetY, width, height)
 	content, cNvPrID, err := f.drawingParser(drawingXML)
 	if err != nil {
 		return err
