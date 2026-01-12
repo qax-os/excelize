qax-os/excelize

commit 47b5c303448bb6b672bd51b01bb317a115ecb4e4

Support set one cell anchor positioning type for shape and slicer
- Update unit tests
- Simplify code with check graphic options in one utility function
- Support get one cell anchor positioning slicers

shape.go
@@ -31,25 +31,19 @@ func parseShapeOptions(opts *Shape) (*Shape, error) {
 	if opts.Height == 0 {
 		opts.Height = defaultShapeSize
 	}
-	if opts.Format.PrintObject == nil {
-		opts.Format.PrintObject = boolPtr(true)
-	}
-	if opts.Format.Locked == nil {
-		opts.Format.Locked = boolPtr(false)
-	}
-	if opts.Format.ScaleX == 0 {
-		opts.Format.ScaleX = defaultDrawingScale
-	}
-	if opts.Format.ScaleY == 0 {
-		opts.Format.ScaleY = defaultDrawingScale
-	}
 	if opts.Line.Width == nil {
 		opts.Line.Width = float64Ptr(defaultShapeLineWidth)
 	}
 	if opts.Fill.Transparency < 0 || 100 < opts.Fill.Transparency {
 		return opts, ErrTransparency
 	}
-	return opts, nil
+	format := opts.Format
+	graphicOptions, err := format.parseGraphicOptions(nil)
+	if err != nil {
+		return opts, err
+	}
+	opts.Format = *graphicOptions
+	return opts, err
 }
 
 // AddShape provides the method to add shape in a sheet by given worksheet
@@ -325,9 +319,9 @@ func (f *File) AddShape(sheet string, opts *Shape) error {
 	return f.addContentTypePart(drawingID, "drawings")
 }
 
-// twoCellAnchorShape create a two cell anchor shape size placeholder for a
+// cellAnchorShape create a two cell anchor shape size placeholder for a
 // group, a shape, or a drawing element.
-func (f *File) twoCellAnchorShape(sheet, drawingXML, cell string, width, height uint, format GraphicOptions) (*xlsxWsDr, *xdrCellAnchor, int, error) {
+func (f *File) cellAnchorShape(sheet, drawingXML, cell string, width, height uint, format GraphicOptions) (*xlsxWsDr, *xdrCellAnchor, int, error) {
 	fromCol, fromRow, err := CellNameToCoordinates(cell)
 	if err != nil {
 		return nil, nil, 0, err
@@ -339,27 +333,36 @@ func (f *File) twoCellAnchorShape(sheet, drawingXML, cell string, width, height 
 	if err != nil {
 		return content, nil, cNvPrID, err
 	}
-	twoCellAnchor := xdrCellAnchor{}
-	twoCellAnchor.EditAs = format.Positioning
+	cellAnchor := xdrCellAnchor{}
+	cellAnchor.EditAs = format.Positioning
 	from := xlsxFrom{}
 	from.Col = colStart
 	from.ColOff = x1 * EMU
 	from.Row = rowStart
 	from.RowOff = y1 * EMU
-	to := xlsxTo{}
-	to.Col = colEnd
-	to.ColOff = x2 * EMU
-	to.Row = rowEnd
-	to.RowOff = y2 * EMU
-	twoCellAnchor.From = &from
-	twoCellAnchor.To = &to
-	return content, &twoCellAnchor, cNvPrID, err
+	cellAnchor.From = &from
+	if format.Positioning != "oneCell" {
+		to := xlsxTo{}
+		to.Col = colEnd
+		to.ColOff = x2 * EMU
+		to.Row = rowEnd
+		to.RowOff = y2 * EMU
+		cellAnchor.To = &to
+		cellAnchor.EditAs = format.Positioning
+	}
+	if format.Positioning == "oneCell" {
+		cellAnchor.Ext = &xlsxPositiveSize2D{
+			Cx: x2 * EMU,
+			Cy: y2 * EMU,
+		}
+	}
+	return content, &cellAnchor, cNvPrID, err
 }
 
 // addDrawingShape provides a function to add preset geometry by given sheet,
 // drawingXML and format sets.
 func (f *File) addDrawingShape(sheet, drawingXML, cell string, opts *Shape) error {
-	content, twoCellAnchor, cNvPrID, err := f.twoCellAnchorShape(
+	content, cellAnchor, cNvPrID, err := f.cellAnchorShape(
 		sheet, drawingXML, cell, opts.Width, opts.Height, opts.Format)
 	if err != nil {
 		return err
@@ -380,6 +383,12 @@ func (f *File) addDrawingShape(sheet, drawingXML, cell string, opts *Shape) erro
 			},
 		},
 		SpPr: &xlsxSpPr{
+			Xfrm: xlsxXfrm{
+				Ext: xlsxPositiveSize2D{
+					Cx: int(opts.Width) * EMU,
+					Cy: int(opts.Height) * EMU,
+				},
+			},
 			PrstGeom: xlsxPrstGeom{
 				Prst: opts.Type,
 			},
@@ -476,12 +485,16 @@ func (f *File) addDrawingShape(sheet, drawingXML, cell string, opts *Shape) erro
 		}
 		shape.TxBody.P = append(shape.TxBody.P, paragraph)
 	}
-	twoCellAnchor.Sp = &shape
-	twoCellAnchor.ClientData = &xdrClientData{
+	cellAnchor.Sp = &shape
+	cellAnchor.ClientData = &xdrClientData{
 		FLocksWithSheet:  *opts.Format.Locked,
 		FPrintsWithSheet: *opts.Format.PrintObject,
 	}
-	content.TwoCellAnchor = append(content.TwoCellAnchor, twoCellAnchor)
+	if opts.Format.Positioning == "oneCell" {
+		content.OneCellAnchor = append(content.OneCellAnchor, cellAnchor)
+	} else {
+		content.TwoCellAnchor = append(content.TwoCellAnchor, cellAnchor)
+	}
 	f.Drawings.Store(drawingXML, content)
 	return err
 }

chart.go
@@ -583,18 +583,6 @@ func parseChartOptions(opts *Chart) (*Chart, error) {
 	if opts.Dimension.Height == 0 {
 		opts.Dimension.Height = defaultChartDimensionHeight
 	}
-	if opts.Format.PrintObject == nil {
-		opts.Format.PrintObject = boolPtr(true)
-	}
-	if opts.Format.Locked == nil {
-		opts.Format.Locked = boolPtr(false)
-	}
-	if opts.Format.ScaleX == 0 {
-		opts.Format.ScaleX = defaultDrawingScale
-	}
-	if opts.Format.ScaleY == 0 {
-		opts.Format.ScaleY = defaultDrawingScale
-	}
 	if opts.Legend.Position == "" {
 		opts.Legend.Position = defaultChartLegendPosition
 	}
@@ -611,6 +599,12 @@ func parseChartOptions(opts *Chart) (*Chart, error) {
 	if opts.ShowBlanksAs == "" {
 		opts.ShowBlanksAs = defaultChartShowBlanksAs
 	}
+	format := opts.Format
+	graphicOptions, err := format.parseGraphicOptions(nil)
+	if err != nil {
+		return opts, err
+	}
+	opts.Format = *graphicOptions
 	return opts, opts.parseSeries()
 }
 

chart_test.go
@@ -151,6 +151,10 @@ func TestAddChart(t *testing.T) {
 
 	// Test add chart on not exists worksheet
 	assert.EqualError(t, f.AddChart("SheetN", "P1", nil), "sheet SheetN does not exist")
+	// Test add chart with invalid positioning types
+	assert.Equal(t, f.AddChart("Sheet1", "P1", &Chart{
+		Format: GraphicOptions{Positioning: "x"},
+	}), newInvalidOptionalValue("Positioning", "x", supportedPositioning))
 	maximum, minimum, zero := 7.5, 0.5, .0
 	series := []ChartSeries{
 		{Name: "Sheet1!$A$30", Categories: "Sheet1!$B$29:$D$29", Values: "Sheet1!$B$30:$D$30"},

picture.go
@@ -38,14 +38,9 @@ const (
 
 // parseGraphicOptions provides a function to parse the format settings of
 // the picture with default value.
-func parseGraphicOptions(opts *GraphicOptions) *GraphicOptions {
+func (opts *GraphicOptions) parseGraphicOptions(defaults *GraphicOptions) (*GraphicOptions, error) {
 	if opts == nil {
-		return &GraphicOptions{
-			PrintObject: boolPtr(true),
-			Locked:      boolPtr(true),
-			ScaleX:      defaultDrawingScale,
-			ScaleY:      defaultDrawingScale,
-		}
+		return defaults, nil
 	}
 	if opts.PrintObject == nil {
 		opts.PrintObject = boolPtr(true)
@@ -59,7 +54,24 @@ func parseGraphicOptions(opts *GraphicOptions) *GraphicOptions {
 	if opts.ScaleY == 0 {
 		opts.ScaleY = defaultDrawingScale
 	}
-	return opts
+	if opts.Positioning != "" && inStrSlice(supportedPositioning, opts.Positioning, true) == -1 {
+		return defaults, newInvalidOptionalValue("Positioning", opts.Positioning, supportedPositioning)
+	}
+	return opts, nil
+}
+
+// parsePictureOptions provides a function to parse the picture options with
+// default value.
+func parsePictureOptions(pic *Picture) (*GraphicOptions, error) {
+	if pic.InsertType != PictureInsertTypePlaceOverCells {
+		return nil, ErrParameterInvalid
+	}
+	return pic.Format.parseGraphicOptions(&GraphicOptions{
+		PrintObject: boolPtr(true),
+		Locked:      boolPtr(true),
+		ScaleX:      defaultDrawingScale,
+		ScaleY:      defaultDrawingScale,
+	})
 }
 
 // AddPicture provides the method to add picture in a sheet by given picture
@@ -237,10 +249,10 @@ func (f *File) AddPictureFromBytes(sheet, cell string, pic *Picture) error {
 	if !ok {
 		return ErrImgExt
 	}
-	if pic.InsertType != PictureInsertTypePlaceOverCells {
-		return ErrParameterInvalid
+	options, err := parsePictureOptions(pic)
+	if err != nil {
+		return err
 	}
-	options := parseGraphicOptions(pic.Format)
 	img, _, err := image.DecodeConfig(bytes.NewReader(pic.File))
 	if err != nil {
 		return err

picture_test.go
@@ -369,8 +369,8 @@ func TestAddPictureFromBytes(t *testing.T) {
 	imgFile, err := os.ReadFile("logo.png")
 	assert.NoError(t, err, "Unable to load logo for test")
 
-	assert.NoError(t, f.AddPictureFromBytes("Sheet1", fmt.Sprint("A", 1), &Picture{Extension: ".png", File: imgFile, Format: &GraphicOptions{AltText: "logo"}}))
-	assert.NoError(t, f.AddPictureFromBytes("Sheet1", fmt.Sprint("A", 50), &Picture{Extension: ".png", File: imgFile, Format: &GraphicOptions{AltText: "logo"}}))
+	assert.NoError(t, f.AddPictureFromBytes("Sheet1", "A1", &Picture{Extension: ".png", File: imgFile, Format: &GraphicOptions{AltText: "logo"}}))
+	assert.NoError(t, f.AddPictureFromBytes("Sheet1", "A50", &Picture{Extension: ".png", File: imgFile, Format: &GraphicOptions{AltText: "logo"}}))
 	imageCount := 0
 	f.Pkg.Range(func(fileName, v interface{}) bool {
 		if strings.Contains(fileName.(string), "media/image") {
@@ -379,9 +379,11 @@ func TestAddPictureFromBytes(t *testing.T) {
 		return true
 	})
 	assert.Equal(t, 1, imageCount, "Duplicate image should only be stored once.")
-	assert.EqualError(t, f.AddPictureFromBytes("SheetN", fmt.Sprint("A", 1), &Picture{Extension: ".png", File: imgFile, Format: &GraphicOptions{AltText: "logo"}}), "sheet SheetN does not exist")
+	assert.EqualError(t, f.AddPictureFromBytes("SheetN", "A1", &Picture{Extension: ".png", File: imgFile}), "sheet SheetN does not exist")
 	// Test add picture from bytes with invalid sheet name
-	assert.EqualError(t, f.AddPictureFromBytes("Sheet:1", fmt.Sprint("A", 1), &Picture{Extension: ".png", File: imgFile, Format: &GraphicOptions{AltText: "logo"}}), ErrSheetNameInvalid.Error())
+	assert.EqualError(t, f.AddPictureFromBytes("Sheet:1", "A1", &Picture{Extension: ".png", File: imgFile}), ErrSheetNameInvalid.Error())
+	// Test add picture from bytes with invalid positioning types
+	assert.Equal(t, f.AddPictureFromBytes("Sheet1", "A1", &Picture{Extension: ".png", File: imgFile, Format: &GraphicOptions{Positioning: "x"}}), newInvalidOptionalValue("Positioning", "x", supportedPositioning))
 }
 
 func TestDeletePicture(t *testing.T) {

shape_test.go
@@ -21,27 +21,11 @@ func TestAddShape(t *testing.T) {
 		},
 	}))
 	assert.NoError(t, f.AddShape("Sheet1", &Shape{Cell: "B30", Type: "rect", Paragraph: []RichTextRun{{Text: "Rectangle"}, {}}}))
-	assert.NoError(t, f.AddShape("Sheet1", &Shape{Cell: "C30", Type: "rect"}))
-	assert.EqualError(t, f.AddShape("Sheet3",
-		&Shape{
-			Cell: "H1",
-			Type: "ellipseRibbon",
-			Line: ShapeLine{Color: "4286F4"},
-			Fill: Fill{Color: []string{"8EB9FF"}},
-			Paragraph: []RichTextRun{
-				{
-					Font: &Font{
-						Bold:      true,
-						Italic:    true,
-						Family:    "Times New Roman",
-						Size:      36,
-						Color:     "777777",
-						Underline: "single",
-					},
-				},
-			},
-		},
-	), "sheet Sheet3 does not exist")
+	shape1 := Shape{Cell: "C30", Type: "rect", Width: 160, Height: 160}
+	assert.NoError(t, f.AddShape("Sheet1", &shape1))
+	// Test add shape with invalid positioning types
+	assert.Equal(t, newInvalidOptionalValue("Positioning", "x", supportedPositioning), f.AddShape("Sheet1", &Shape{Cell: "C30", Type: "rect", Format: GraphicOptions{Positioning: "x"}}))
+	assert.EqualError(t, f.AddShape("Sheet3", &Shape{Cell: "C30", Type: "rect"}), "sheet Sheet3 does not exist")
 	assert.Equal(t, ErrParameterInvalid, f.AddShape("Sheet3", nil))
 	assert.Equal(t, ErrParameterInvalid, f.AddShape("Sheet1", &Shape{Cell: "A1"}))
 	assert.Equal(t, newCellNameToCoordinatesError("A", newInvalidCellNameError("A")), f.AddShape("Sheet1", &Shape{
@@ -57,26 +41,34 @@ func TestAddShape(t *testing.T) {
 	// Test add first shape for given sheet
 	f = NewFile()
 	lineWidth := 1.2
-	assert.NoError(t, f.AddShape("Sheet1",
-		&Shape{
-			Cell: "A1",
-			Type: "ellipseRibbon",
-			Line: ShapeLine{Color: "4286F4", Width: &lineWidth},
-			Fill: Fill{Color: []string{"8EB9FF"}, Transparency: 60},
-			Paragraph: []RichTextRun{
-				{
-					Font: &Font{
-						Bold:      true,
-						Italic:    true,
-						Family:    "Times New Roman",
-						Size:      36,
-						Color:     "777777",
-						Underline: "single",
-					},
+	shape2 := Shape{
+		Cell: "A1",
+		Type: "ellipseRibbon",
+		Line: ShapeLine{Color: "4286F4", Width: &lineWidth},
+		Fill: Fill{Color: []string{"8EB9FF"}, Transparency: 60},
+		Format: GraphicOptions{
+			PrintObject: boolPtr(true),
+			Locked:      boolPtr(false),
+			ScaleX:      0.8,
+			ScaleY:      0.8,
+			Positioning: "oneCell",
+		},
+		Paragraph: []RichTextRun{
+			{
+				Font: &Font{
+					Bold:      true,
+					Italic:    true,
+					Family:    "Times New Roman",
+					Size:      18,
+					Color:     "777777",
+					Underline: "sng",
 				},
+				Text: "Shape",
 			},
-			Height: 90,
-		}))
+		},
+		Height: 90,
+	}
+	assert.NoError(t, f.AddShape("Sheet1", &shape2))
 	assert.NoError(t, f.SaveAs(filepath.Join("test", "TestAddShape2.xlsx")))
 	// Test add shape with invalid sheet name
 	assert.Equal(t, ErrSheetNameInvalid, f.AddShape("Sheet:1", &Shape{

slicer.go
@@ -137,19 +137,13 @@ func parseSlicerOptions(opts *SlicerOptions) (*SlicerOptions, error) {
 	if opts.Height == 0 {
 		opts.Height = defaultSlicerHeight
 	}
-	if opts.Format.PrintObject == nil {
-		opts.Format.PrintObject = boolPtr(true)
+	format := opts.Format
+	graphicOptions, err := format.parseGraphicOptions(nil)
+	if err != nil {
+		return opts, err
 	}
-	if opts.Format.Locked == nil {
-		opts.Format.Locked = boolPtr(false)
-	}
-	if opts.Format.ScaleX == 0 {
-		opts.Format.ScaleX = defaultDrawingScale
-	}
-	if opts.Format.ScaleY == 0 {
-		opts.Format.ScaleY = defaultDrawingScale
-	}
-	return opts, nil
+	opts.Format = *graphicOptions
+	return opts, err
 }
 
 // countSlicers provides a function to get slicer files count storage in the
@@ -600,7 +594,7 @@ func (f *File) addDrawingSlicer(sheet, slicerName string, ns xml.Attr, opts *Sli
 		return err
 	}
 	drawingID, drawingXML = f.prepareDrawing(ws, drawingID, sheet, drawingXML)
-	content, twoCellAnchor, cNvPrID, err := f.twoCellAnchorShape(sheet, drawingXML, opts.Cell, opts.Width, opts.Height, opts.Format)
+	content, cellAnchor, cNvPrID, err := f.cellAnchorShape(sheet, drawingXML, opts.Cell, opts.Width, opts.Height, opts.Format)
 	if err != nil {
 		return err
 	}
@@ -648,7 +642,7 @@ func (f *File) addDrawingSlicer(sheet, slicerName string, ns xml.Attr, opts *Sli
 		},
 	}
 	shape, _ := xml.Marshal(sp)
-	twoCellAnchor.ClientData = &xdrClientData{
+	cellAnchor.ClientData = &xdrClientData{
 		FLocksWithSheet:  *opts.Format.Locked,
 		FPrintsWithSheet: *opts.Format.PrintObject,
 	}
@@ -662,11 +656,16 @@ func (f *File) addDrawingSlicer(sheet, slicerName string, ns xml.Attr, opts *Sli
 	fallback := xlsxFallback{Content: string(shape)}
 	choiceBytes, _ := xml.Marshal(choice)
 	shapeBytes, _ := xml.Marshal(fallback)
-	twoCellAnchor.AlternateContent = append(twoCellAnchor.AlternateContent, &xlsxAlternateContent{
+	cellAnchor.AlternateContent = append(cellAnchor.AlternateContent, &xlsxAlternateContent{
 		XMLNSMC: SourceRelationshipCompatibility.Value,
 		Content: string(choiceBytes) + string(shapeBytes),
 	})
-	content.TwoCellAnchor = append(content.TwoCellAnchor, twoCellAnchor)
+	if opts.Format.Positioning == "oneCell" {
+		content.OneCellAnchor = append(content.OneCellAnchor, cellAnchor)
+	} else {
+		content.TwoCellAnchor = append(content.TwoCellAnchor, cellAnchor)
+	}
+
 	f.Drawings.Store(drawingXML, content)
 	return f.addContentTypePart(drawingID, "drawings")
 }
@@ -896,56 +895,88 @@ func (f *File) extractPivotTableSlicer(slicerCache *xlsxSlicerCacheDefinition, o
 			opt.ItemDesc = slicerCache.Data.Tabular.SortOrder == "descending"
 		}
 	}
-	return nil
+	return err
 }
 
 // extractSlicerCellAnchor extract slicer drawing object from two cell anchor by
 // giving drawing part path and slicer options.
 func (f *File) extractSlicerCellAnchor(drawingXML string, opt *SlicerOptions) error {
 	var (
-		wsDr         *xlsxWsDr
-		deCellAnchor = new(decodeCellAnchor)
-		deChoice     = new(decodeChoice)
-		err          error
+		wsDr *xlsxWsDr
+		err  error
 	)
 	if wsDr, _, err = f.drawingParser(drawingXML); err != nil {
 		return err
 	}
 	wsDr.mu.Lock()
 	defer wsDr.mu.Unlock()
-	cond := func(ac *xlsxAlternateContent) bool {
+	for _, anchor := range wsDr.OneCellAnchor {
+		if err = f.extractSlicerFromAnchor(anchor, opt); err != nil {
+			return err
+		}
+		if err = f.extractSlicerFromDecodeAnchor(anchor, opt); err != nil {
+			return err
+		}
+	}
+	for _, anchor := range wsDr.TwoCellAnchor {
+		if err = f.extractSlicerFromAnchor(anchor, opt); err != nil {
+			return err
+		}
+		if err = f.extractSlicerFromDecodeAnchor(anchor, opt); err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+// extractSlicerFromAnchor extract slicer drawing object from cell anchor by
+// giving drawing part path and slicer options.
+func (f *File) extractSlicerFromAnchor(anchor *xdrCellAnchor, opt *SlicerOptions) error {
+	var (
+		deChoice = new(decodeChoice)
+		err      error
+	)
+	for _, ac := range anchor.AlternateContent {
 		if ac != nil {
 			_ = f.xmlNewDecoder(strings.NewReader(ac.Content)).Decode(&deChoice)
 			if deChoice.XMLNSSle15 == NameSpaceDrawingMLSlicerX15.Value || deChoice.XMLNSA14 == NameSpaceDrawingMLA14.Value {
 				if deChoice.GraphicFrame.NvGraphicFramePr.CNvPr.Name == opt.Name {
-					return true
+					if anchor.From != nil {
+						opt.Macro = deChoice.GraphicFrame.Macro
+						if opt.Cell, err = CoordinatesToCellName(anchor.From.Col+1, anchor.From.Row+1); err != nil {
+							return err
+						}
+					}
+					return err
 				}
 			}
 		}
-		return false
 	}
-	for _, anchor := range wsDr.TwoCellAnchor {
-		for _, ac := range anchor.AlternateContent {
-			if cond(ac) {
-				if anchor.From != nil {
-					opt.Macro = deChoice.GraphicFrame.Macro
-					if opt.Cell, err = CoordinatesToCellName(anchor.From.Col+1, anchor.From.Row+1); err != nil {
-						return err
+	return err
+}
+
+// extractSlicerFromDecodeAnchor extract slicer drawing object from decode cell
+// anchor by giving drawing part path and slicer options.
+func (f *File) extractSlicerFromDecodeAnchor(anchor *xdrCellAnchor, opt *SlicerOptions) error {
+	var (
+		deCellAnchor = new(decodeCellAnchor)
+		deChoice     = new(decodeChoice)
+		err          error
+	)
+	_ = f.xmlNewDecoder(strings.NewReader("<decodeCellAnchor>" + anchor.GraphicFrame + "</decodeCellAnchor>")).Decode(&deCellAnchor)
+	for _, ac := range deCellAnchor.AlternateContent {
+		if ac != nil {
+			_ = f.xmlNewDecoder(strings.NewReader(ac.Content)).Decode(&deChoice)
+			if deChoice.XMLNSSle15 == NameSpaceDrawingMLSlicerX15.Value || deChoice.XMLNSA14 == NameSpaceDrawingMLA14.Value {
+				if deChoice.GraphicFrame.NvGraphicFramePr.CNvPr.Name == opt.Name {
+					if deCellAnchor.From != nil {
+						opt.Macro = deChoice.GraphicFrame.Macro
+						if opt.Cell, err = CoordinatesToCellName(deCellAnchor.From.Col+1, deCellAnchor.From.Row+1); err != nil {
+							return err
+						}
 					}
+					return err
 				}
-				return err
-			}
-		}
-		_ = f.xmlNewDecoder(strings.NewReader("<decodeCellAnchor>" + anchor.GraphicFrame + "</decodeCellAnchor>")).Decode(&deCellAnchor)
-		for _, ac := range deCellAnchor.AlternateContent {
-			if cond(ac) {
-				if deCellAnchor.From != nil {
-					opt.Macro = deChoice.GraphicFrame.Macro
-					if opt.Cell, err = CoordinatesToCellName(deCellAnchor.From.Col+1, deCellAnchor.From.Row+1); err != nil {
-						return err
-					}
-				}
-				return err
 			}
 		}
 	}

slicer_test.go
@@ -45,6 +45,7 @@ func TestSlicer(t *testing.T) {
 		Height:        200,
 		DisplayHeader: &disable,
 		ItemDesc:      true,
+		Format:        GraphicOptions{Positioning: "oneCell"},
 	}))
 	// Test get table slicers
 	slicers, err := f.GetSlicers("Sheet1")
@@ -174,6 +175,14 @@ func TestSlicer(t *testing.T) {
 		TableSheet: "SheetN",
 		TableName:  "Table1",
 	}), "sheet SheetN does not exist")
+	// Test add slicer with invalid positioning types
+	assert.Equal(t, f.AddSlicer("Sheet1", &SlicerOptions{
+		Name:       "Column2",
+		Cell:       "Q1",
+		TableSheet: "Sheet1",
+		TableName:  "Table1",
+		Format:     GraphicOptions{Positioning: "x"},
+	}), newInvalidOptionalValue("Positioning", "x", supportedPositioning))
 	// Test add a table slicer with not exist table name
 	assert.Equal(t, newNoExistTableError("Table2"), f.AddSlicer("Sheet1", &SlicerOptions{
 		Name:       "Column2",
@@ -399,9 +408,17 @@ func TestSlicer(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Empty(t, slicers)
 	assert.NoError(t, f.Close())
-	// Test open a workbook and get sheet slicer with invalid cell reference in the drawing part
+	// Test open a workbook and get sheet slicer with invalid cell reference in the drawing one cell anchor
 	f, err = OpenFile(workbookPath)
 	assert.NoError(t, err)
+	f.Pkg.Store("xl/drawings/drawing1.xml", []byte(fmt.Sprintf(`<wsDr xmlns="%s"><oneCellAnchor><from><col>-1</col><row>-1</row></from><mc:AlternateContent><mc:Choice xmlns:sle15="%s"><graphicFrame><nvGraphicFramePr><cNvPr id="2" name="Column1"/></nvGraphicFramePr></graphicFrame></mc:Choice></mc:AlternateContent></oneCellAnchor></wsDr>`, NameSpaceDrawingMLSpreadSheet.Value, NameSpaceDrawingMLSlicerX15.Value)))
+	_, err = f.GetSlicers("Sheet1")
+	assert.Equal(t, newCoordinatesToCellNameError(0, 0), err)
+	assert.NoError(t, f.Close())
+	// Test open a workbook and get sheet slicer with invalid cell reference in the drawing one cell anchor
+	f, err = OpenFile(workbookPath)
+	assert.NoError(t, err)
+	// Test open a workbook and get sheet slicer with invalid cell reference in the drawing two cell anchor
 	f.Pkg.Store("xl/drawings/drawing1.xml", []byte(fmt.Sprintf(`<wsDr xmlns="%s"><twoCellAnchor><from><col>-1</col><row>-1</row></from><mc:AlternateContent><mc:Choice xmlns:sle15="%s"><graphicFrame><nvGraphicFramePr><cNvPr id="2" name="Column1"/></nvGraphicFramePr></graphicFrame></mc:Choice></mc:AlternateContent></twoCellAnchor></wsDr>`, NameSpaceDrawingMLSpreadSheet.Value, NameSpaceDrawingMLSlicerX15.Value)))
 	_, err = f.GetSlicers("Sheet1")
 	assert.Equal(t, newCoordinatesToCellNameError(0, 0), err)
@@ -424,8 +441,27 @@ func TestSlicer(t *testing.T) {
 	_, err = f.GetSlicers("Sheet2")
 	assert.EqualError(t, err, "XML syntax error on line 1: invalid UTF-8")
 	assert.NoError(t, f.Close())
-
-	// Test create a workbook and get sheet slicer with invalid cell reference in the drawing part
+	// Test create a workbook and get sheet slicer with invalid cell reference in the drawing one cell anchor
+	f = NewFile()
+	assert.NoError(t, f.AddTable("Sheet1", &Table{
+		Name:  "Table1",
+		Range: "A1:D5",
+	}))
+	assert.NoError(t, f.AddSlicer("Sheet1", &SlicerOptions{
+		Name:       "Column1",
+		Cell:       "E1",
+		TableSheet: "Sheet1",
+		TableName:  "Table1",
+		Caption:    "Column1",
+		Format:     GraphicOptions{Positioning: "oneCell"},
+	}))
+	drawing, ok := f.Drawings.Load("xl/drawings/drawing1.xml")
+	assert.True(t, ok)
+	drawing.(*xlsxWsDr).OneCellAnchor[0].From = &xlsxFrom{Col: -1, Row: -1}
+	_, err = f.GetSlicers("Sheet1")
+	assert.Equal(t, newCoordinatesToCellNameError(0, 0), err)
+	assert.NoError(t, f.Close())
+	// Test create a workbook and get sheet slicer with invalid cell reference in the drawing two cell anchor
 	f = NewFile()
 	assert.NoError(t, f.AddTable("Sheet1", &Table{
 		Name:  "Table1",
@@ -438,7 +474,7 @@ func TestSlicer(t *testing.T) {
 		TableName:  "Table1",
 		Caption:    "Column1",
 	}))
-	drawing, ok := f.Drawings.Load("xl/drawings/drawing1.xml")
+	drawing, ok = f.Drawings.Load("xl/drawings/drawing1.xml")
 	assert.True(t, ok)
 	drawing.(*xlsxWsDr).TwoCellAnchor[0].From = &xlsxFrom{Col: -1, Row: -1}
 	_, err = f.GetSlicers("Sheet1")

commit 800dd0fcbab4ffddc50dda0861f1347fe906509e

Fix light color theme index overflow caused workbook corrupted
- Fix a regression bug introduced by commit 47b5c303448bb6b672bd51b01bb317a115ecb4e4 and 8a99debc18f1ce5087ae36c0b77a328357dafa78
- Remove editAs attribute for one cell anchor
- Update dependencies modules
- Update document for stream writer functions

drawing.go
@@ -884,13 +884,13 @@ func (f *File) drawChartSeriesDPt(i int, opts *Chart) []*cDPt {
 				W:   25400,
 				Cap: "rnd",
 				SolidFill: &aSolidFill{
-					SchemeClr: &aSchemeClr{Val: "lt" + strconv.Itoa(i+1)},
+					SchemeClr: &aSchemeClr{Val: "lt1"},
 				},
 			},
 			Sp3D: &aSp3D{
 				ContourW: 25400,
 				ContourClr: &aContourClr{
-					SchemeClr: &aSchemeClr{Val: "lt" + strconv.Itoa(i+1)},
+					SchemeClr: &aSchemeClr{Val: "lt1"},
 				},
 			},
 		},

go.mod
@@ -11,7 +11,7 @@ require (
 	golang.org/x/crypto v0.46.0
 	golang.org/x/image v0.25.0
 	golang.org/x/net v0.48.0
-	golang.org/x/text v0.32.0
+	golang.org/x/text v0.33.0
 )
 
 require (

go.sum
@@ -20,8 +20,8 @@ golang.org/x/image v0.25.0 h1:Y6uW6rH1y5y/LK1J8BPWZtr6yZ7hrsy6hFrXjgsc2fQ=
 golang.org/x/image v0.25.0/go.mod h1:tCAmOEGthTtkalusGp1g3xa2gke8J6c2N565dTyl9Rs=
 golang.org/x/net v0.48.0 h1:zyQRTTrjc33Lhh0fBgT/H3oZq9WuvRR5gPC70xpDiQU=
 golang.org/x/net v0.48.0/go.mod h1:+ndRgGjkh8FGtu1w1FGbEC31if4VrNVMuKTgcAAnQRY=
-golang.org/x/text v0.32.0 h1:ZD01bjUt1FQ9WJ0ClOL5vxgxOI/sVCNgX1YtKwcY0mU=
-golang.org/x/text v0.32.0/go.mod h1:o/rUWzghvpD5TXrTIBuJU77MTaN0ljMWE47kxGJQ7jY=
+golang.org/x/text v0.33.0 h1:B3njUFyqtHDUI5jMn1YIr5B0IE2U0qck04r6d4KPAxE=
+golang.org/x/text v0.33.0/go.mod h1:LuMebE6+rBincTi9+xWTY8TztLzKHc/9C1uBCG27+q8=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 h1:yhCVgyC4o1eVCa2tZl7eS0r+SDo693bJlVdllGtEeKM=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
 gopkg.in/yaml.v3 v3.0.1 h1:fxVm/GzAzEWqLHuvctI91KS9hhNmmWOoWu0XTYJS7CA=

shape.go
@@ -334,7 +334,6 @@ func (f *File) cellAnchorShape(sheet, drawingXML, cell string, width, height uin
 		return content, nil, cNvPrID, err
 	}
 	cellAnchor := xdrCellAnchor{}
-	cellAnchor.EditAs = format.Positioning
 	from := xlsxFrom{}
 	from.Col = colStart
 	from.ColOff = x1 * EMU

stream.go
@@ -439,7 +439,7 @@ func (sw *StreamWriter) SetRow(cell string, values []interface{}, opts ...RowOpt
 // multiple columns for the StreamWriter. Note that you must call the
 // 'SetColVisible' function before the 'SetRow' function.
 //
-// For example hide column D on Sheet1:
+// For example hide column D:
 //
 //	err := sw.SetColVisible(4, 4, false)
 //
@@ -461,8 +461,9 @@ func (sw *StreamWriter) SetColVisible(minVal, maxVal int, visible bool) error {
 }
 
 // SetColOutlineLevel provides a function to set outline level of a single
-// column for the StreamWriter. The value of parameter 'level' is 1-7. For
-// example, set outline level of column D in Sheet1 to 2:
+// column for the StreamWriter. The value of parameter 'level' is 1-7. Note that
+// you must call the 'SetColOutlineLevel' function before the 'SetRow' function.
+// For example, set outline level of column D to 2:
 //
 //	err := sw.SetColOutlineLevel(4, 2)
 func (sw *StreamWriter) SetColOutlineLevel(col int, level uint8) error {
@@ -482,7 +483,7 @@ func (sw *StreamWriter) SetColOutlineLevel(col int, level uint8) error {
 // SetColStyle provides a function to set the style of a single column or
 // multiple columns for the StreamWriter. Note that you must call
 // the 'SetColStyle' function before the 'SetRow' function. For example set
-// style of column H on Sheet1:
+// style of column H:
 //
 //	err := sw.SetColStyle(8, 8, style)
 func (sw *StreamWriter) SetColStyle(minVal, maxVal, styleID int) error {