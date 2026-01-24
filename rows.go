qax-os/excelize

commit a33bd1aa5b086ee8fb4237e2c7aed2c614b20828

This closes #2019, closes #2150, fix panic on get string item with invalid offset range
- Update unit test

rows.go
@@ -355,6 +355,9 @@ func (f *File) getFromStringItem(index int) string {
 			return strconv.Itoa(index)
 		}
 		offsetRange := f.sharedStringItem[index]
+		if len(offsetRange) != 2 || offsetRange[0] >= offsetRange[1] {
+			return strconv.Itoa(index)
+		}
 		buf := make([]byte, offsetRange[1]-offsetRange[0])
 		if _, err := f.sharedStringTemp.ReadAt(buf, int64(offsetRange[0])); err != nil {
 			return strconv.Itoa(index)

cell_test.go
@@ -1178,6 +1178,9 @@ func TestSharedStringsError(t *testing.T) {
 	assert.True(t, ok)
 	f.tempFiles.Store(defaultXMLPathSharedStrings, "")
 	assert.Equal(t, "1", f.getFromStringItem(1))
+	// Test get from string item with invalid offset range
+	f.sharedStringItem = [][]uint{{0}}
+	assert.Equal(t, "0", f.getFromStringItem(0))
 	// Cleanup undelete temporary files
 	assert.NoError(t, os.Remove(tempFile.(string)))
 	// Test reload the file error on set cell value and rich text. The error message was different between macOS and Windows

commit 2dcfb6085068ad414d49df4ce965488b7945455a

This closes #2240, fix GetCellValue returning shared string index for empty strings (#2252)
- Fix a v2.10.0 regression bug introduced by commit a33bd1aa5b086ee8fb4237e2c7aed2c614b20828

rows.go
@@ -355,7 +355,7 @@ func (f *File) getFromStringItem(index int) string {
 			return strconv.Itoa(index)
 		}
 		offsetRange := f.sharedStringItem[index]
-		if len(offsetRange) != 2 || offsetRange[0] >= offsetRange[1] {
+		if len(offsetRange) != 2 || offsetRange[0] > offsetRange[1] {
 			return strconv.Itoa(index)
 		}
 		buf := make([]byte, offsetRange[1]-offsetRange[0])