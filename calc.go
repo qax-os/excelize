f958f2f4c66d5b95583930bb5be98c955887db5a
Rainyz <1989133140@qq.com>
2026-02-21 09:38:45 +0800

    fix: prevent panic on invalid row reference in CalcCellValue

--- calc.go
+++ calc.go
@@ -1541,6 +1541,9 @@ func parseRef(ref string) (cellRef, bool, bool, error) {
 			return cr, true, false, nil
 		}
 		if cr.Row, rowErr = strconv.Atoi(cell); rowErr == nil { // cast to row
+			if cr.Row < 1 {
+				return cr, false, false, fmt.Errorf("invalid row number %q", cell)
+			}
 			return cr, false, true, nil
 		}
 		return cr, false, false, err
@@ -1777,7 +1780,7 @@ func (f *File) rangeResolver(ctx *calcContext, cellRefs, cellRanges *list.List) 
 
 		for row := valueRange[0]; row <= valueRange[1]; row++ {
 			colMax := 0
-			if row <= len(ws.SheetData.Row) {
+			if row >= 1 && row <= len(ws.SheetData.Row) {
 				rowData := &ws.SheetData.Row[row-1]
 				colMax = min(valueRange[3], len(rowData.C))
 			}
