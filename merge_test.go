qax-os/excelize

commit 6cc1a547abd4ed47a61536d97b47e00383faf512

This closes #1712, reduce memory consumption (#1713)

sheet.go
@@ -178,36 +178,39 @@ func (f *File) workSheetWriter() {
 // trimRow provides a function to trim empty rows.
 func trimRow(sheetData *xlsxSheetData) []xlsxRow {
 	var (
-		row  xlsxRow
-		rows []xlsxRow
+		row xlsxRow
+		i   int
 	)
-	for k, v := range sheetData.Row {
+
+	for k := range sheetData.Row {
 		row = sheetData.Row[k]
-		if row.C = trimCell(v.C); len(row.C) != 0 || row.hasAttr() {
-			rows = append(rows, row)
+		if row = trimCell(row); len(row.C) != 0 || row.hasAttr() {
+			sheetData.Row[i] = row
 		}
+		i++
 	}
-	return rows
+	return sheetData.Row[:i]
 }
 
 // trimCell provides a function to trim blank cells which created by fillColumns.
-func trimCell(column []xlsxC) []xlsxC {
+func trimCell(row xlsxRow) xlsxRow {
+	column := row.C
 	rowFull := true
 	for i := range column {
 		rowFull = column[i].hasValue() && rowFull
 	}
 	if rowFull {
-		return column
+		return row
 	}
-	col := make([]xlsxC, len(column))
 	i := 0
 	for _, c := range column {
 		if c.hasValue() {
-			col[i] = c
+			row.C[i] = c
 			i++
 		}
 	}
-	return col[:i]
+	row.C = row.C[:i]
+	return row
 }
 
 // setContentTypes provides a function to read and update property of contents

commit 7722fac1cff231f3dd2e3e4797b390664df91a85

This compact sparse worksheet rows in linear time (Go 1.21 and later required)
- This fixes issue #1383 regression caused by #1713 introduced in v2.8.1
- This fixes performance regression caused by #2228, fixes #2226 in better way
- Simplify unit tests and added unit test cases for issues #1383 and #2226

merge_test.go
@@ -1,7 +1,10 @@
 package excelize
 
 import (
+	"encoding/xml"
+	"fmt"
 	"path/filepath"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -106,6 +109,20 @@ func TestMergeCellOverlap(t *testing.T) {
 	assert.Equal(t, "D3", mc[0].GetEndAxis())
 	assert.Empty(t, mc[0].GetCellValue())
 	assert.NoError(t, f.Close())
+
+	t.Run("for_worksheet_with_max_cells", func(t *testing.T) {
+		f := NewFile()
+		f.Sheet.Delete("xl/worksheets/sheet1.xml")
+		worksheet := xml.Header + `<worksheet xmlns="%s"><sheetData><row r="1"><c r="A1"><v>1</v></c></row></sheetData><mergeCells count="1"><mergeCell ref="A1:XFD1048576"/></mergeCells></worksheet>`
+		f.Pkg.Store("xl/worksheets/sheet1.xml", fmt.Appendf(nil, worksheet, NameSpaceSpreadSheet.Value))
+		f.checked = sync.Map{}
+		mergeCells, err := f.GetMergeCells("Sheet1")
+		assert.NoError(t, err)
+		mergeCell := mergeCells[0]
+		assert.Equal(t, "A1", mergeCell.GetStartAxis())
+		assert.Equal(t, "XFD1048576", mergeCell.GetEndAxis())
+		assert.NoError(t, f.Close())
+	})
 }
 
 func TestGetMergeCells(t *testing.T) {

sheet_test.go
@@ -520,6 +520,18 @@ func TestWorksheetWriter(t *testing.T) {
 	value, ok := f.Pkg.Load("xl/worksheets/sheet1.xml")
 	assert.True(t, ok)
 	assert.Equal(t, fmt.Sprintf(worksheet, 2), string(value.([]byte)))
+
+	t.Run("for_worksheet_with_max_rows", func(t *testing.T) {
+		f := NewFile()
+		f.Sheet.Delete("xl/worksheets/sheet1.xml")
+		worksheet := xml.Header + `<worksheet xmlns="%s"><dimension ref="A1048576"/><sheetData><row r="1048576" spans="1:1"><c r="A1048576" s="0"/></row></sheetData></worksheet>`
+		f.Pkg.Store("xl/worksheets/sheet1.xml", fmt.Appendf(nil, worksheet, NameSpaceSpreadSheet.Value))
+		f.checked = sync.Map{}
+		assert.NoError(t, f.SetCellValue("Sheet1", "A1", 1))
+		f.workSheetWriter()
+		assert.Equal(t, fmt.Sprintf(xml.Header+`<worksheet xmlns="%s" xmlns:r="%s"><dimension ref="A1048576"></dimension><sheetData><row r="1"><c r="A1"><v>1</v></c></row><row r="1048576" spans="1:1"></row></sheetData></worksheet>`, NameSpaceSpreadSheet.Value, SourceRelationship.Value), string(f.readXML("xl/worksheets/sheet1.xml")))
+		assert.NoError(t, f.Close())
+	})
 }
 
 func TestGetWorkbookPath(t *testing.T) {